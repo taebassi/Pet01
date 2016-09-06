f9671450d8dacfa38d936ab5c180514721996084
s2pAIr <samostzz@gmail.com>
2016-09-07 02:57:53 +0700

    Clear LinearLayout and add Guest Login

--- app/src/main/java/com/example/taeba/pet01/MainActivity.java
+++ app/src/main/java/com/example/taeba/pet01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.taeba.pet01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvGuestLogin = (TextView)findViewById(R.id.tvGuestLogin);
+        tvGuestLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GuestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
