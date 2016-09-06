329ff448c62d19551aca39c9e23eb9294b1706e0
s2pAIr <samostzz@gmail.com>
2016-09-07 04:19:07 +0700

    comment code

--- app/src/main/java/com/example/taeba/pet01/RegisActivity.java
+++ app/src/main/java/com/example/taeba/pet01/RegisActivity.java
@@ -1,13 +1,28 @@
 package com.example.taeba.pet01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regis);
+        //setContentView(R.layout.activity_regis);
+        //String fullname = findViewById(R.id.editTextUsername).toString();
+        //String username = findViewById(R.id.editTextUsername).toString();
+        //String email = findViewById(R.id.editTextEmail).toString();
+        //String password = findViewById(R.id.editTextPassword).toString();
+        Button btRegister = (Button)findViewById(R.id.btRegister);
+        /*btRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(RegisActivity.this,RegisterResaultActivity.class);
+                    startActivity(intent);
+            }
+        });*/
     }
 }
---
+++ app/src/main/java/com/example/taeba/pet01/RegisterResaultActivity.java
@@ -0,0 +1,25 @@
+package com.example.taeba.pet01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterResaultActivity extends AppCompatActivity {
+
+    String fullname;
+    String username;
+    String email;
+    String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_resault);
+
+        //Intent intent = new Intent();
+        //intent.getStringExtra("fullname");
+        //intent.getStringExtra("username");
+        //intent.getStringExtra("email");
+        //intent.getStringExtra("password");
+    }
+}
