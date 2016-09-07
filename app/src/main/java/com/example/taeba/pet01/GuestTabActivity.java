e11248128b7f58acc9a598ae7836d8bbcfa0d979
taebassi <athanon18@hotmail.com>
2016-09-07 22:36:06 +0700

    tae(7/9/59 22:35)

--- app/src/main/java/com/example/taeba/pet01/GuestTabActivity.java
+++ app/src/main/java/com/example/taeba/pet01/GuestTabActivity.java
@@ -66,14 +66,14 @@ public class GuestTabActivity extends AppCompatActivity {
     }
 
 
-    @Override
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_guest_tab, menu);
         return true;
     }
 
-    @Override
+   /@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
